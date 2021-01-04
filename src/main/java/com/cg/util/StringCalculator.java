57f0add0588664f99f318a496fb5d9dd25fde893
Apichai Tangmansujaritkul <TanApichai@central.co.th>
2021-01-05 00:15:09 +0700

    Support different delimiters.

--- src/main/java/com/cg/util/StringCalculator.java
+++ src/main/java/com/cg/util/StringCalculator.java
@@ -6,9 +6,22 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-    public int Add(String numbers) {
+    public int add(String numbers) {
         if (StringUtils.hasLength(numbers)) {
-            String[] numbersArray = numbers.split(",");
+            String defaultDelimiter = ",";
+            boolean isDefaultDelimiter = true;
+
+            if (numbers.startsWith("//")) {
+                defaultDelimiter = numbers.substring(2, 3);
+                numbers = numbers.substring(3);
+                isDefaultDelimiter = false;
+            }
+
+            numbers = numbers.replace(System.lineSeparator(), defaultDelimiter);
+            String[] numbersArray = numbers.split(defaultDelimiter);
+            if (!isDefaultDelimiter)
+                numbersArray[0] = String.valueOf(0);
+
             return Arrays.stream(numbersArray).map(Integer::valueOf).reduce(Integer::sum).get();
         } else {
             return 0;
--- src/test/java/com/cg/util/StringCalculatorTest.java
+++ src/test/java/com/cg/util/StringCalculatorTest.java
@@ -8,32 +8,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    private StringCalculator calculator;
+
     @BeforeEach
     void setUp() {
+        calculator = new StringCalculator();
     }
 
     @AfterEach
     void tearDown() {
+        calculator = null;
     }
 
     @Test
-    void addWhenNumbersIsEmptyShouldReturnZero() {
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.Add("");
+    void add_When_TheNumbersIsEmpty_Then_ReturnZero() {
+        int result = calculator.add("");
         assertEquals(0, result);
     }
 
     @Test
-    void addWhenNumbersIsOneShouldReturnOne() {
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.Add("1");
+    void add_When_TheNumbersIsOne_Then_ReturnOne() {
+        int result = calculator.add("1");
         assertEquals(1, result);
     }
 
     @Test
-    void addWhenNumbersIsOneAndTwoShouldReturnThree() {
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.Add("1,2");
+    void add_When_TheNumbersIsOneAndTwo_Then_ReturnThree() {
+        int result = calculator.add("1,2");
+        assertEquals(3, result);
+    }
+
+    @Test
+    void add_Given_NewLinesAsDelimiter_When_TheNumbersContainsNewLinesChar_Then_ReturnSix() {
+        int result = calculator.add("1\n2,3");
+        assertEquals(6, result);
+    }
+
+    @Test
+    void add_Given_SemiColonDelimiter_When_TheNumbersContainsDelimiterAndOneAndTwo_Then_ReturnSix() {
+        int result = calculator.add("//;\n1;2");
         assertEquals(3, result);
     }
 }
